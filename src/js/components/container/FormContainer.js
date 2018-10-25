d328b4af23c0db3dba23823967fa350071fe6a41
HimanshuP90 <himanshup895@gmail.com>
2018-10-25 22:37:26 +0530

    fetch added

--- src/js/components/container/FormContainer.js
+++ src/js/components/container/FormContainer.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom";
 import Input from "../presentational/Input";
 import Paragraph from "../presentational/Paragraph";
 import ErrorBoundary from "./ErrorBoundary";
+import style from "../../../../src/main.css";
+
 class FormContainer extends Component {
   constructor() {
     super();
     this.state = {
       react_title: '',
       customer_name: 'Himanshu Pandey',
-      jsonData:''
+      jsonData:'',
+      status: '',
+      author: '',
+      newsData: ''
     };
   }
   
@@ -19,15 +24,27 @@ class FormContainer extends Component {
       .then(json => this.setState({jsonData: json}))
   }
 
+  componentDidMount(){
+    fetch('https://newsapi.org/v1/articles?source=techcrunch&apiKey=03ad4cda66c547818f091bfa57522413')
+      .then((res) => {
+        res.json().then((data) => {
+          this.setState({ newsData: data })
+        })
+      })
+  }
+
   handleChange(event) {
     this.setState({ [event.target.id]: event.target.value });
   }
   render() {
-    const { react_title, customer_name, jsonData } = this.state;
+    const { react_title, customer_name, jsonData, newsData } = this.state;
     const obj = jsonData && jsonData.map((obj) => { return obj.title })
     const p = `When we create event handler method, we always need to add this to constructor,
     to bind this. Quite tiresome. To be honest, there is no sense to create constructor
     method only for binding your method ${customer_name}`
+
+    const news = newsData && newsData.articles.map((val) => { return val})
+    console.log(news);
     return (
       <>
       <form id="article-form">
@@ -41,7 +58,7 @@ class FormContainer extends Component {
         />
       </form>
       <ErrorBoundary>
-        <Paragraph p={obj}/>
+        <Paragraph p={p}/>
       </ErrorBoundary>
       </>
     );
