301b0335b2812109d932fef45e0820ef4fbc12ab
HimanshuP90 <himanshup895@gmail.com>
2018-11-03 19:19:15 +0530

    map filter added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import FormContainer from "./js/components/container/FormContainer";
+
+const numbers = [2, 4, 8, 10];
+const halsves = numbers.map(x => x/2);
+
+console.log(halsves);
+
+const filters = numbers.filter(x => x>2);
+console.log(filters);
