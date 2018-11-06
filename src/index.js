55de14b7a44cdfd6d5b3dcc3d2571178aa1775db
HimanshuP90 <himanshup895@gmail.com>
2018-11-06 21:17:14 +0530

    added flatten an array of arrays

--- src/index.js
+++ src/index.js
@@ -4,23 +4,32 @@ import FormContainer from "./js/components/container/FormContainer";
 const numbers = [2, 4, 8, 10];
 const halsves = numbers.map(x => x/2);
 
-console.log(halsves);
+// console.log(halsves);
 
 //Array.prototype.filter()
 const filters = numbers.filter(x => x>2);
-console.log(filters);
+// cons ole.log(filters);
 
 //Array.prototype.reduce()
 // The reduce() method executes a reducer function on each member
 // of the array resulting in a single array
 
 
-const array1 = [9, 8, 7, 6];
-console.log([9, 8, 7, 6])
+const array1 = [9];
+// console.log([9, 8, 7, 6])
 const reducer = (accumulator, currentValue) => { 
     console.log('accumulator: ', accumulator)
     console.log('currentValue: ', currentValue);
     console.log('accumulator + currentValue = ', accumulator + currentValue)
     return accumulator + currentValue
 };
-console.log(array1.reduce(reducer));
+// console.log(array1.reduce(reducer));
+
+//Flatten an array of arrays
+var flattened = [[0,1], [2,3], [4,5]].reduce(
+    function(accumulator, currentValue) {
+        return accumulator.concat(currentValue);
+    },
+    []
+);
+console.log(flattened)
